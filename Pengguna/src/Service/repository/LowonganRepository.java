1d5e3078e9d09816767752a337c4625b8819eac6
gnafhan <nafhanghifari@gmail.com>
2024-07-05 01:26:24 +0700

    add method join lowongan and get all lowongan

--- Pengguna/src/Service/repository/LowonganRepository.java
+++ Pengguna/src/Service/repository/LowonganRepository.java
@@ -1,9 +1,11 @@
 package Service.repository;
 
+import Models.Lowongan;
 import Service.Config;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class LowonganRepository {
     public static boolean joinLowonganDB(int idLowongan, int idPengguna, String status, String namaLengkap, String gender, String asalDaerah, String pendidikanTerakhir, String tentangSaya) throws SQLException {
@@ -21,5 +23,27 @@ public class LowonganRepository {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public static ArrayList<Lowongan> getAllLowonganDB(){
+        Connection connection = null;
+        try {
+            connection = Config.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            var result = connection.createStatement().executeQuery("SELECT * FROM lowongan");
+            ArrayList<Lowongan> lowongans = new ArrayList<>();
+            while (result.next()) {
+                Lowongan newLowongan = new Lowongan(result.getString("posisi"), result.getString("perusahaan"), result.getString("lokasi"), result.getInt("gaji"), result.getString("kualifikai"), result.getString("waktuDiposting"), result.getString("jenisWaktu"), result.getString("jobDesk"), result.getString("tanggungJawab"));
+                newLowongan.setId(result.getInt("id"));
+                lowongans.add(newLowongan);
+            }
+            return lowongans;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
