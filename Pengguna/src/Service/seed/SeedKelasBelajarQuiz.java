3986c6c183b7bccb7076c80844315fed4cf27415
gnafhan <nafhanghifari@gmail.com>
2024-07-08 07:23:45 +0700

    fix seed pelatihan pengguna

--- Pengguna/src/Service/seed/SeedKelasBelajarQuiz.java
+++ Pengguna/src/Service/seed/SeedKelasBelajarQuiz.java
@@ -10,8 +10,8 @@ public class SeedKelasBelajarQuiz {
         try {
             Connection connection = Config.getConnection();
             truncateKelasBelajarQuiz();
-            connection.createStatement().executeUpdate("INSERT INTO kelas_belajar_quiz (idKelas, orderQuiz, namaQuiz) VALUES (1, 2, 'Quiz Wortel')");
-            connection.createStatement().executeUpdate("INSERT INTO kelas_belajar_quiz (idKelas, orderQuiz, namaQuiz) VALUES (1, 4, 'Quiz Tomat')");
+            connection.createStatement().executeUpdate("INSERT INTO kelas_belajar_quiz (idKelas, orderQuiz, namaQuiz) VALUES (1, 1, 'Quiz Wortel')");
+            connection.createStatement().executeUpdate("INSERT INTO kelas_belajar_quiz (idKelas, orderQuiz, namaQuiz) VALUES (1, 2, 'Quiz Tomat')");
 
             System.out.println("Seed Kelas Belajar Quiz berhasil");
             connection.close();
